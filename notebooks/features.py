ed872e973c519edea5c5575bc27bcecaf4a32fea
apuri99 <andrea3.puricelli@mail.polimi.it>
2023-05-30 12:52:56 +0200

    Update final

--- notebooks/features.py
+++ notebooks/features.py
@@ -3,50 +3,28 @@ import numpy as np
 
 class Features():
 
-    # def calc_fft(self, signal, fs = 200):
-    #     """ This functions computes the fft of a signal."""
-    #     print(self)
-    #     print(signal)
-    #     fmag = np.abs(np.fft.fft(signal))
-    #     f = np.linspace(0, fs // 2, len(signal) // 2)
+    # def autocorr(self, signal):
+    #     """Computes autocorrelation of the signal."""
+    #     signal = np.array(signal)
+    #     return float(np.correlate(signal, signal))
 
-     #   return f[:len(signal) // 2].copy(), fmag[:len(signal) // 2].copy()
+    # def negative_turning(self, signal):
+    #     """Computes number of negative turning points of the signal."""
+    #     diff_sig = np.diff(signal)
+    #     array_signal = np.arange(len(diff_sig[:-1]))
+    #     negative_turning_pts = np.where((diff_sig[array_signal] < 0) & (diff_sig[array_signal + 1] > 0))[0]
 
-    def autocorr(self, signal):
-        """Computes autocorrelation of the signal."""
-        signal = np.array(signal)
-        return float(np.correlate(signal, signal))
+    #     return len(negative_turning_pts)
 
-    # skip
-    # def calc_centroid(signal, fs):
-    #     """Computes the centroid along the time axis."""
-    #     time = compute_time(signal, fs)
-    #     energy = np.array(signal) ** 2
-    #     t_energy = np.dot(np.array(time), np.array(energy))
-    #     energy_sum = np.sum(energy)
-    #     if energy_sum == 0 or t_energy == 0:
-    #         centroid = 0
-    #     else:
-    #         centroid = t_energy / energy_sum
-    #     return centroid
+    # def positive_turning(self, signal):
+    #     """Computes number of positive turning points of the signal."""
+    #     diff_sig = np.diff(signal)
 
-    def negative_turning(self, signal):
-        """Computes number of negative turning points of the signal."""
-        diff_sig = np.diff(signal)
-        array_signal = np.arange(len(diff_sig[:-1]))
-        negative_turning_pts = np.where((diff_sig[array_signal] < 0) & (diff_sig[array_signal + 1] > 0))[0]
+    #     array_signal = np.arange(len(diff_sig[:-1]))
 
-        return len(negative_turning_pts)
+    #     positive_turning_pts = np.where((diff_sig[array_signal + 1] < 0) & (diff_sig[array_signal] > 0))[0]
 
-    def positive_turning(self, signal):
-        """Computes number of positive turning points of the signal."""
-        diff_sig = np.diff(signal)
-
-        array_signal = np.arange(len(diff_sig[:-1]))
-
-        positive_turning_pts = np.where((diff_sig[array_signal + 1] < 0) & (diff_sig[array_signal] > 0))[0]
-
-        return len(positive_turning_pts)
+    #     return len(positive_turning_pts)
 
     def mean_abs_diff(self, signal):
         """Computes mean absolute differences of the signal."""
@@ -64,11 +42,11 @@ class Features():
         """Computes median of differences of the signal."""
         return np.median(np.diff(signal))
 
-    def distance(self, signal): # è la lunghezza bro
-        """Calculates the total distance traveled by the signal
-        using the hipotenusa between 2 datapoints."""
-        diff_sig = np.diff(signal).astype(float)
-        return np.sum([np.sqrt(1 + diff_sig ** 2)])
+    # def distance(self, signal): # è la lunghezza bro
+    #     """Calculates the total distance traveled by the signal
+    #     using the hipotenusa between 2 datapoints."""
+    #     diff_sig = np.diff(signal).astype(float)
+    #     return np.sum([np.sqrt(1 + diff_sig ** 2)])
 
     def sum_abs_diff(self, signal):
         """Computes sum of absolute differences of the signal."""
@@ -98,53 +76,13 @@ class Features():
         """Computes the peak to peak distance."""
         return np.abs(np.max(signal) - np.min(signal))
 
-    # def entropy(signal, prob='standard'):
-    #     """Computes the entropy of the signal using the Shannon Entropy."""
-
-    #     if prob == 'standard':
-    #         value, counts = np.unique(signal, return_counts=True)
-    #         p = counts / counts.sum()
-    #     elif prob == 'kde':
-    #         p = kde(signal)
-    #     elif prob == 'gauss':
-    #         p = gaussian(signal)
-
-    #     if np.sum(p) == 0:
-    #         return 0.0
-
-    #     # Handling zero probability values
-    #     p = p[np.where(p != 0)]
-
-    #     # If probability all in one value, there is no entropy
-    #     if np.log2(len(signal)) == 1:
-    #         return 0.0
-    #     elif np.sum(p * np.log2(p)) / np.log2(len(signal)) == 0:
-    #         return 0.0
-    #     else:
-    #         return - np.sum(p * np.log2(p)) / np.log2(len(signal))
-
-    # def neighbourhood_peaks(signal, n=10):
-    #     """Computes the number of peaks from a defined neighbourhood of the signal."""
-    #     signal = np.array(signal)
-    #     subsequence = signal[n:-n]
-    #     # initial iteration
-    #     peaks = ((subsequence > np.roll(signal, 1)[n:-n]) & (subsequence > np.roll(signal, -1)[n:-n]))
-    #     for i in range(2, n + 1):
-    #         peaks &= (subsequence > np.roll(signal, i)[n:-n])
-    #         peaks &= (subsequence > np.roll(signal, -i)[n:-n])
-    #     return np.sum(peaks)
-
-    def interq_range(self, signal):
-        """Computes interquartile range of the signal."""
-        return np.percentile(signal, 75) - np.percentile(signal, 25)
+    # def interq_range(self, signal):
+    #     """Computes interquartile range of the signal."""
+    #     return np.percentile(signal, 75) - np.percentile(signal, 25)
 
     def kurtosis(self, signal):
         """Computes kurtosis of the signal."""
         return scipy.stats.kurtosis(signal)
-    
-    # def skewness(self, signal):
-    #     """Computes skewness of the signal."""
-    #     return scipy.stats.skew(signal)
     
     def calc_max(self, signal):
         """Computes the maximum value of the signal."""
@@ -177,50 +115,6 @@ class Features():
     def calc_var(self, signal):
         """Computes variance of the signal."""
         return np.var(signal)
-    
-    #def ecdf(self, signal, d=10):
-    #    """Computes the values of ECDF (empirical cumulative distribution function) along the time axis."""
-    #    _, y = np.sort(signal), np.arange(1, len(signal)+1)/len(signal)
-    #    if len(signal) <= d:
-    #        return tuple(y)
-    #    else:
-    #        return tuple(y[:d])
-    
-    #def ecdf_percentile(self, signal, percentile=[0.2, 0.8]):
-    #    """Computes the percentile values of the ECDF."""
-    #    signal = np.array(signal)
-    #    if isinstance(percentile, str):
-    #        percentile = eval(percentile)
-    #    if isinstance(percentile, (float, int)):
-    #        percentile = [percentile]
-#
-    #    # calculate ecdf
-    #    x, y = np.sort(signal), np.arange(1, len(signal)+1)/len(signal)
-#
-    #    if len(percentile) > 1:
-    #        # check if signal is constant
-    #        if np.sum(np.diff(signal)) == 0:
-    #            return tuple(np.repeat(signal[0], len(percentile)))
-    #        else:
-    #            return tuple([x[y <= p].max() for p in percentile])
-    #    else:
-    #        # check if signal is constant
-    #        if np.sum(np.diff(signal)) == 0:
-    #            return signal[0]
-    #        else:
-    #            return x[y <= percentile].max()
-
-    
-    # def spectral_distance(signal, fs):
-    #     """Computes the signal spectral distance."""
-    #     f, fmag = calc_fft(signal, fs)
-
-    #     cum_fmag = np.cumsum(fmag)
-
-    #     # Computing the linear regression
-    #     points_y = np.linspace(0, cum_fmag[-1], len(cum_fmag))
-
-    #     return np.sum(points_y - cum_fmag)
 
     # TODO: reinserire
     # def fundamental_frequency(signal, fs):
@@ -244,100 +138,3 @@ class Features():
     #         f0 = f[min(bp)]
 
     #     return f0
-    
-    # def max_frequency(signal, fs):
-    #     """Computes maximum frequency of the signal."""
-    #     f, fmag = calc_fft(signal, fs)
-    #     cum_fmag = np.cumsum(fmag)
-
-    #     try:
-    #         ind_mag = np.where(cum_fmag > cum_fmag[-1] * 0.95)[0][0]
-    #     except IndexError:
-    #         ind_mag = np.argmax(cum_fmag)
-
-    #     return f[ind_mag]
-    
-    # def median_frequency(signal, fs):
-    #     """Computes median frequency of the signal."""
-    #     f, fmag = calc_fft(signal, fs)
-    #     cum_fmag = np.cumsum(fmag)
-    #     try:
-    #         ind_mag = np.where(cum_fmag > cum_fmag[-1] * 0.50)[0][0]
-    #     except IndexError:
-    #         ind_mag = np.argmax(cum_fmag)
-    #     f_median = f[ind_mag]
-
-    #     return f_median
-    
-    # def spectral_decrease(signal, fs):
-    #     """Represents the amount of decreasing of the spectra amplitude."""
-    #     f, fmag = calc_fft(signal, fs)
-
-    #     fmag_band = fmag[1:]
-    #     len_fmag_band = np.arange(2, len(fmag) + 1)
-
-    #     # Sum of numerator
-    #     soma_num = np.sum((fmag_band - fmag[0]) / (len_fmag_band - 1), axis=0)
-
-    #     if not np.sum(fmag_band):
-    #         return 0
-    #     else:
-    #         # Sum of denominator
-    #         soma_den = 1 / np.sum(fmag_band)
-
-    #         # Spectral decrease computing
-    #         return soma_den * soma_num
-
-    
-    # def spectral_variation(signal, fs):
-    #     """Computes the amount of variation of the spectrum along time.
-
-    #     Spectral variation is computed from the normalized cross-correlation between two consecutive amplitude spectra.
-
-    #     Description and formula in Article:
-    #     The Timbre Toolbox: Extracting audio descriptors from musicalsignals
-    #     Authors Peeters G., Giordano B., Misdariis P., McAdams S.
-    #     """
-    #     f, fmag = calc_fft(signal, fs)
-
-    #     sum1 = np.sum(np.array(fmag)[:-1] * np.array(fmag)[1:])
-    #     sum2 = np.sum(np.array(fmag)[1:] ** 2)
-    #     sum3 = np.sum(np.array(fmag)[:-1] ** 2)
-
-    #     if not sum2 or not sum3:
-    #         variation = 1
-    #     else:
-    #         variation = 1 - (sum1 / ((sum2 ** 0.5) * (sum3 ** 0.5)))
-
-    #     return variation
-    
-    # def human_range_energy(signal, fs):
-    #     """Computes the human range energy ratio.
-    #     The human range energy ratio is given by the ratio between the energy
-    #     in frequency 0.6-2.5Hz and the whole energy band.
-    #     """
-    #     f, fmag = calc_fft(signal, fs)
-
-    #     allenergy = np.sum(fmag ** 2)
-
-    #     if allenergy == 0:
-    #         # For handling the occurrence of Nan values
-    #         return 0.0
-
-    #     hr_energy = np.sum(fmag[np.argmin(np.abs(0.6 - f)):np.argmin(np.abs(2.5 - f))] ** 2)
-
-    #     ratio = hr_energy / allenergy
-
-    #    return ratio
-    
-    #def fft_mean_coeff(self, signal, fs=200, nfreq=256):
-    #    """Computes the mean value of each spectrogram frequency.
-    #    nfreq can not be higher than half signal length plus one.
-    #    When it does, it is automatically set to half signal length plus one.
-    #    """
-    #    if nfreq > len(signal) // 2 + 1:
-    #        nfreq = len(signal) // 2 + 1
-#
-    #    fmag_mean = scipy.signal.spectrogram(signal, fs, nperseg=nfreq * 2 - 2)[2].mean(1)
-#
-    #    return tuple(fmag_mean)
